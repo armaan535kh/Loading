ddf3470fc6d6aa45ae6e335b5fd9d37e27d33ef3
armaan535kh <kh.armaan535@gmail.com>
2024-11-10 10:10:58 +0530

    Initial Commit

---
+++ Loading/LoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingApp.swift
+//  Loading
+//
+//  Created by Armaan Khan  on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoadingTests/LoadingTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingTests.swift
+//  LoadingTests
+//
+//  Created by Armaan Khan  on 10/11/24.
+//
+
+import Testing
+@testable import Loading
+
+struct LoadingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
